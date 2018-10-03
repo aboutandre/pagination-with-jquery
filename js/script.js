b073011514a51676b8def3375d0d7c338d6b4a3b
aboutandre <github@aboutandre.com>
2018-10-03 19:07:38 +0200

    Add pagination

--- js/script.js
+++ js/script.js
@@ -7,18 +7,33 @@ FSJS project 2 - List Filter and Pagination
 const $filter = $('<input type="text" />');
 const $header = $('.page-header.cf');
 const $allStudents = $('.student-item.cf');
+const $studentsCount = $allStudents.length;
+const studentsPerPage = 10;
+const pagesTotal = Math.round($studentsCount / studentsPerPage) + 1;
 
-// Create a function to hide all of the items in the list excpet for the ten you want to show
-// Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
+console.log($studentsCount);
+// Create a function to hide all of the items in the list except for the ten you want to show
+// Tip: Keep in mind that with a list of 54 students, the last page will only display four
+$('.student-item:gt('+ (studentsPerPage - 1 )+')').hide();
 
 
 
 
 // Create and append the pagination links - Creating a function that can do this is a good approach
+const createPagination = function() {
+    $('.pagination').remove();
+    let pagination = '';
+    pagination += '<ul class="pagination">';
+    for (let i = 1; i < pagesTotal; i++) {
+        let paginationItem = '<li><a href="#/" class="button-page">' + i + '</a></li>';
+        pagination += paginationItem;
+    }
+    pagination += '</ul>';
+    $('.page').append(pagination);
+}
 
-
-
-
+createPagination();
+    
 // Add functionality to the pagination buttons so that they show and hide the correct items
 // Tip: If you created a function above to show/hide list items, it could be helpful here
 
@@ -37,4 +52,5 @@ $filter.keyup(function(){
     
     $allStudents.hide(); // We hide all students
     $matchingStudents.show(); // We show only the students that match the search query
+    createPagination();
 });
