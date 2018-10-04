b19d74ac5357a4dbebcc2729aecc835b43ce15dc
aboutandre <github@aboutandre.com>
2018-10-04 22:06:39 +0200

    [experimental/new-approach]  ADD a working app with filter and pagination working Last todo for this project is to refactor the code and add comments to the file

--- js/script.js
+++ js/script.js
@@ -6,51 +6,57 @@ FSJS project 2 - List Filter and Pagination
 // Add variables that store DOM elements you will need to reference and/or manipulate
 const $header = $('.page-header.cf');
 const $studentItem = $('.student-item');
+const $studentItemNotFiltered = $('.student-item').not('.filtered');
 const $studentDetails = $('.student-details h3');
 const $studentAmmout = $studentItem.length;
 const limitPerPage = 5;
 
 // Create a function to hide all of the items in the list excpet for the ten you want to show
 // Tip: Keep in mind that with a list of 54 students, the last page will only display four
-const showPage = (page = 0, list = $studentItem) => {
-    list.each(function (index, element) {
+const showPage = (list = $('.student-item'), page = 0) => {
+    console.log('How many have not been filtered out: ' + $('.student-item').not('.filtered').length);
+    list.each(function (index) {
         let rangeLow = (page * limitPerPage);
         let rangeHigh = (page * limitPerPage) + limitPerPage;
         if (index >= rangeLow && index < rangeHigh) {
             // console.log(index);
-            $(element).show();
+            $(this).show();
         } else {
             // console.log('This should be hidden: ' + index);
-            $(element).hide();
+            $(this).hide();
         }
     })
 };
 
 // Create and append the pagination links - Creating a function that can do this is a good approach
-const createPagination = (list) => {
+const createPagination = (totalAmmount = $studentAmmout) => {
     $('.pagination').remove();
-    let pagesTotal = Math.round($studentAmmout / limitPerPage) + 1;
+    let pagesTotal = Math.round(totalAmmount / limitPerPage) + 1;
     // console.log('The total pages is ' + (pagesTotal - 1));
     let pagination = '';
-    pagination += '<ul class="pagination"><li><a href="#/" class="button-page active">1</a></li>';
-    for (let i = 2; i < pagesTotal; i++) {
-        let paginationItem = '<li><a href="#/" class="button-page">' + i + '</a></li>';
-        pagination += paginationItem;
+    if (pagesTotal <= 2) {
+        return;
+    } else {
+        pagination += '<ul class="pagination"><li><a href="#/" class="button-page active">1</a></li>';
+        for (let i = 2; i < pagesTotal; i++) {
+            let paginationItem = '<li><a href="#/" class="button-page">' + i + '</a></li>';
+            pagination += paginationItem;
+        }
+        pagination += '</ul>';
+        $('.page').append(pagination);
     }
-    pagination += '</ul>';
-    $('.page').append(pagination);
 }
 
 // Add functionality to the pagination buttons so that they show and hide the correct items
 // Tip: If you created a function above to show/hide list items, it could be helpful here
-const navigatePage = () => {
+const navigatePage = (list = $('.student-item')) => {
     $('.button-page').on('click', function () {
         if ($(this).hasClass === 'active') {
             return false;
         } else {
             $('.button-page').removeClass('active');
             $(this).addClass('active');
-            showPage($(this).parent().index());
+            showPage(list, $(this).parent().index());
             // console.log($(this).parent().index());
         }
     })
@@ -68,31 +74,30 @@ const filter = `<div class="student-search">
 `;
 $header.append(filter);
 
-(function ($) {
-    // custom css expression for a case-insensitive contains()
-    jQuery.expr[':'].Contains = function (a, i, m) {
-        return (a.textContent || a.innerText || "").toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
+// Make jQuery :contains Case-Insensitive - via "https://css-tricks.com/snippets/jquery/make-jquery-contains-case-insensitive/"
+$.expr[":"].contains = $.expr.createPseudo(function (arg) {
+    return function (elem) {
+        return $(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
     };
+});
 
-    function listFilter(list) {
-        $('.search-box')
-            .change(function () {
-                var filter = $(this).val();
-                if (filter) {
-                    // this finds all links in a list that contain the input,
-                    // and hide the ones not containing the input while showing the ones that do
-                    $(list).find(".student-details h3:not(:Contains(" + filter + "))").parent().parent().slideUp();
-                    $(list).find(".student-details h3:Contains(" + filter + ")").parent().parent().slideDown();
-                } else {
-                    $(list).find(".student-details h3:Contains(" + filter + ")").parent().parent().slideDown();
-                }
-                return false;
-            })
-            .keyup(function () {
-                // fire the above change event after every letter
-                $(this).change();
-            });
-    }
+function listFilter(list) {
+    $('.search-box').on('keyup', function () {
 
-    listFilter($('.student-list'));
-}(jQuery));
+        var filter = $(this).val();
+        if (filter) {
+            // this finds all links in a list that contain the input,
+            // and hide the ones not containing the input while showing the ones that do
+            $(list).find(".student-details h3:not(:contains(" + filter + "))").parent().parent().removeClass('filtered').hide();
+            $(list).find(".student-details h3:contains(" + filter + ")").parent().parent().addClass('filtered').show();
+        } else {
+            $(list).find(".student-details h3:contains(" + filter + ")").parent().parent().addClass('filtered').hide();
+        }
+        showPage($('.student-item.filtered'));
+        createPagination($('.student-item.filtered').length);
+        navigatePage($('.student-item.filtered'));
+        // return false;
+    })
+}
+
+listFilter($('.student-list'));
