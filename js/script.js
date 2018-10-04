c2b962bdc4d011931f611c152b1f9af6d8e526ff
aboutandre <github@aboutandre.com>
2018-10-04 18:55:22 +0200

    [experimental/new-approach]  ADD filter. However this is not working in conjunction with the pagination. This needs to be addressed.

--- js/script.js
+++ js/script.js
@@ -4,9 +4,9 @@ FSJS project 2 - List Filter and Pagination
 ******************************************/
 
 // Add variables that store DOM elements you will need to reference and/or manipulate
-const $filter = $('<input />');
 const $header = $('.page-header.cf');
 const $studentItem = $('.student-item');
+const $studentDetails = $('.student-details h3');
 const $studentAmmout = $studentItem.length;
 const limitPerPage = 5;
 
@@ -61,4 +61,38 @@ createPagination();
 navigatePage();
 
 // Search
-$header.append($filter);
+const filter = `<div class="student-search">
+                    <input placeholder="Search for students..." class="search-box" />
+                    <button class="search-button">Search</button>
+                </div>
+`;
+$header.append(filter);
+
+(function ($) {
+    // custom css expression for a case-insensitive contains()
+    jQuery.expr[':'].Contains = function (a, i, m) {
+        return (a.textContent || a.innerText || "").toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
+    };
+
+    function listFilter(list) {
+        $('.search-box')
+            .change(function () {
+                var filter = $(this).val();
+                if (filter) {
+                    // this finds all links in a list that contain the input,
+                    // and hide the ones not containing the input while showing the ones that do
+                    $(list).find(".student-details h3:not(:Contains(" + filter + "))").parent().parent().slideUp();
+                    $(list).find(".student-details h3:Contains(" + filter + ")").parent().parent().slideDown();
+                } else {
+                    $(list).find(".student-details h3:Contains(" + filter + ")").parent().parent().slideDown();
+                }
+                return false;
+            })
+            .keyup(function () {
+                // fire the above change event after every letter
+                $(this).change();
+            });
+    }
+
+    listFilter($('.student-list'));
+}(jQuery));
