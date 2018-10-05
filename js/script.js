ebe9b320e0b74e497b701cbf59b0bbc1f82a3eca
aboutandre <github@aboutandre.com>
2018-10-05 17:18:34 +0200

    [experimental/new-approach] ADD styling for the "zero" results box REFACTOR the code

--- js/script.js
+++ js/script.js
@@ -6,29 +6,27 @@ FSJS project 2 - List Filter and Pagination
 // Add variables that store DOM elements you will need to reference and/or manipulate
 const $header = $('.page-header.cf');
 const $studentItem = $('.student-item');
-const $studentItemNotFiltered = $('.student-item').not('.filtered');
-const $studentDetails = $('.student-details h3');
 const $studentAmmout = $studentItem.length;
-const limitPerPage = 5;
+const limitPerPage = 10;
+const noResults = `<div class="no-results"><h2>Oh no!</h2><p>Your search returned <em>no results</em> =( </p><p>Redefine your search query</p></div>`;
+const filter = `<div class="student-search"><input placeholder="Search for students..." class="search-box" /></div>`;
 
 // Create a function to hide all of the items in the list excpet for the ten you want to show
 // Tip: Keep in mind that with a list of 54 students, the last page will only display four
 const showPage = (list = $('.student-item'), page = 0) => {
-    console.log('How many have not been filtered out: ' + $('.student-item').not('.filtered').length);
     list.each(function (index) {
+        $('.no-results').remove();
         let rangeLow = (page * limitPerPage);
         let rangeHigh = (page * limitPerPage) + limitPerPage;
         if (index >= rangeLow && index < rangeHigh) {
-            // console.log(index);
             $(this).show();
         } else {
-            // console.log('This should be hidden: ' + index);
             $(this).hide();
         }
     })
-    // TODO 
-    // When a search yields 0 results, a message is displayed on the page, 
-    // informing the user that no results have been found.
+    if ($('.student-item.filtered').length == $studentAmmout && !$('.no-results').length > 0) {
+        $('.page').append(noResults);
+    }
 };
 
 // Create and append the pagination links - Creating a function that can do this is a good approach
@@ -60,21 +58,11 @@ const navigatePage = (list = $('.student-item')) => {
             $('.button-page').removeClass('active');
             $(this).addClass('active');
             showPage(list, $(this).parent().index());
-            // console.log($(this).parent().index());
         }
     })
 }
 
-showPage();
-createPagination();
-navigatePage();
-
 // Search
-const filter = `<div class="student-search">
-                    <input placeholder="Search for students..." class="search-box" />
-                    <button class="search-button">Search</button>
-                </div>
-`;
 $header.append(filter);
 
 // Make jQuery :contains Case-Insensitive - via "https://css-tricks.com/snippets/jquery/make-jquery-contains-case-insensitive/"
@@ -86,21 +74,23 @@ $.expr[":"].contains = $.expr.createPseudo(function (arg) {
 
 function listFilter(list) {
     $('.search-box').on('keyup', function () {
-
         var filter = $(this).val();
         if (filter) {
             // this finds all links in a list that contain the input,
             // and hide the ones not containing the input while showing the ones that do
-            $(list).find(".student-details h3:not(:contains(" + filter + "))").parent().parent().removeClass('filtered').hide();
-            $(list).find(".student-details h3:contains(" + filter + ")").parent().parent().addClass('filtered').show();
+            $(list).find(".student-details h3:not(:contains(" + filter + "))").parent().parent().addClass('filtered').removeClass('is-visible').hide();
+            $(list).find(".student-details h3:contains(" + filter + ")").parent().parent().removeClass('filtered').addClass('is-visible').show();
         } else {
-            $(list).find(".student-details h3:contains(" + filter + ")").parent().parent().addClass('filtered').hide();
+            $(list).find(".student-details h3:contains(" + filter + ")").parent().parent().removeClass('filtered').addClass('is-visible').show();
         }
-        showPage($('.student-item.filtered'));
-        createPagination($('.student-item.filtered').length);
-        navigatePage($('.student-item.filtered'));
-        // return false;
+        showPage($('.student-item.is-visible'));
+        createPagination($('.student-item.is-visible').length);
+        navigatePage($('.student-item.is-visible'));
+        // console.log('How many have not been filtered out: ' + $('.student-item').not('.filtered').length);
     })
 }
 
+showPage();
+createPagination();
+navigatePage();
 listFilter($('.student-list'));
